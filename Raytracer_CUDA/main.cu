ea52762c790e38afa3cf9bad81c4aa72a974811a
Joey <klein.winz@web.de>
2020-03-23 12:52:24 +0100

    [BASIC] implemented full diffuse material.

--- Raytracer_CUDA/main.cu
+++ Raytracer_CUDA/main.cu
@@ -6,6 +6,7 @@
 #define GLM_FORCE_CUDA
 #include <glm/glm/glm.hpp>
 #include <glm/glm/ext/scalar_constants.hpp>
+#include <glm/glm/gtx/norm.hpp>
 #include <iostream>
 #include <time.h>
 #include <float.h>
@@ -17,9 +18,9 @@
 #include "HittableList.h"
 #include "Sphere.h"
 
-#define IMG_WIDTH 200
-#define IMG_HEIGHT 100
-#define NUM_SAMPLES 10
+#define IMG_WIDTH 1920
+#define IMG_HEIGHT 1080
+#define NUM_SAMPLES 100
 #define TX 8
 #define TY 8
 
@@ -36,36 +37,56 @@ void checkCuda(cudaError_t result, char const* const func, const char* const fil
 	}
 }
 
-// calculates the color of one ray
- __device__ glm::vec3 color(const Ray &ray, Hittable **world)
+__device__ glm::vec3 randomInUnitSphere(curandState* localRandState)
 {
+	glm::vec3 randomPoint;
+	do
+	{
+		randomPoint = 2.0f * glm::vec3(curand_uniform(localRandState), curand_uniform(localRandState), curand_uniform(localRandState)) - glm::vec3(1.0f, 1.0f, 1.0f);
+	} while (glm::length2(randomPoint) >= 1.0f);
 
-	hitRecord rec;
-	if ((*world)->hit(ray, 0.0, FLT_MAX, rec)) {
-		glm::vec3 normalColor = 0.5f * glm::vec3(rec.normal.x + 1.0f, rec.normal.y + 1.0f, rec.normal.z + 1.0f);
-		
-		//glm::vec2 texCoordinate;
-		// In this particular case, the normal is similar to a point on a unit sphere
-		// centred around the origin. We can thus use the normal coordinates to compute
-		// the spherical coordinates of Phit.
-		// atan2 returns a value in the range [-pi, pi] and we need to remap it to range [0, 1]
-		// acosf returns a value in the range [0, pi] and we also need to remap it to the range [0, 1]
-		//texCoordinate.x = (1 + atan2(rec.normal.z, rec.normal.x) / glm::pi<float>()) * 0.5;
-		//texCoordinate.y = acosf(rec.normal.y) / glm::pi<float>();
-
-		//float scale = 4;
-		//float pattern = (fmodf(texCoordinate.x * scale, 1) > 0.5) ^ (fmodf(texCoordinate.y * scale, 1) > 0.5);
-
-		//return glm::max(0.f, glm::dot(rec.normal, -glm::normalize(ray.direction()))) * glm::mix(normalColor, normalColor * 0.8f, pattern);
-		return normalColor;
-	}
-	else {
-		glm::vec3 direction = glm::normalize(ray.direction());
-		float t = 0.5f * (direction.y + 1.0f);
-		return (1.0f - t) * glm::vec3(1.0, 1.0, 1.0) + t * glm::vec3(0.5, 0.7, 1.0);
-	}
+	return randomPoint;
 }
 
+// calculates the color of one ray
+ __device__ glm::vec3 color(const Ray &ray, Hittable **world, curandState *localRandState)
+{
+	Ray currentRay = ray;
+	float currentAttenuation = 1.0f;
+
+	for (int i = 0; i < 50; i++)
+	{
+		hitRecord rec;
+		if ((*world)->hit(currentRay, 0.0001f, FLT_MAX, rec)) {
+			glm::vec3 target = rec.p + rec.normal + randomInUnitSphere(localRandState);
+			currentAttenuation *= 0.5f;
+			currentRay = Ray(rec.p, target - rec.p);
+			//glm::vec3 normalColor = 0.5f * glm::vec3(rec.normal.x + 1.0f, rec.normal.y + 1.0f, rec.normal.z + 1.0f);
+
+			//glm::vec2 texCoordinate;
+			// In this particular case, the normal is similar to a point on a unit sphere
+			// centred around the origin. We can thus use the normal coordinates to compute
+			// the spherical coordinates of Phit.
+			// atan2 returns a value in the range [-pi, pi] and we need to remap it to range [0, 1]
+			// acosf returns a value in the range [0, pi] and we also need to remap it to the range [0, 1]
+			//texCoordinate.x = (1 + atan2(rec.normal.z, rec.normal.x) / glm::pi<float>()) * 0.5;
+			//texCoordinate.y = acosf(rec.normal.y) / glm::pi<float>();
+
+			//float scale = 4;
+			//float pattern = (fmodf(texCoordinate.x * scale, 1) > 0.5) ^ (fmodf(texCoordinate.y * scale, 1) > 0.5);
+
+			//return glm::max(0.f, glm::dot(rec.normal, -glm::normalize(ray.direction()))) * glm::mix(normalColor, normalColor * 0.8f, pattern);
+		}
+		else {
+			glm::vec3 direction = glm::normalize(ray.direction());
+			float t = 0.5f * (direction.y + 1.0f);
+			return currentAttenuation * (1.0f - t) * glm::vec3(1.0, 1.0, 1.0) + t * glm::vec3(0.5, 0.7, 1.0);
+		}
+	}
+	return glm::vec3(0.0f, 0.0f, 0.0f); // Recursion limit exceeded
+}
+
+ // initializes the random function for every thread
  __global__ void renderInit(int width, int height, curandState* randState)
  {
 	 int i = threadIdx.x + blockIdx.x * blockDim.x;
@@ -96,7 +117,7 @@ __global__ void render(glm::vec3* frameBuffer, int width, int height, int numSam
 		float u = float(i + curand_uniform(&localRandState)) / float(width);
 		float v = float(j + curand_uniform(&localRandState)) / float(height);
 		Ray ray = (*camera)->getRay(u, v);
-		col += color(ray, world);
+		col += color(ray, world, &localRandState);
 	}
 	frameBuffer[pixelIndex] = col / float(numSamples);
 }
@@ -135,9 +156,6 @@ int main()
 	start = clock();
 	glm::vec3* frameBuffer;
 	checkCudaErrors(cudaMallocManaged((void**)&frameBuffer, frameBufferSize));
-	stop = clock();
-	std::cout << "\t\t" << (stop - start) << "ms\n";
-	
 
 	Hittable **_dList;
 	checkCudaErrors(cudaMalloc((void**) &_dList, 2 * sizeof(Hittable*)));
@@ -145,6 +163,9 @@ int main()
 	checkCudaErrors(cudaMalloc((void**) &_dWorld, sizeof(Hittable*)));
 	Camera **_dCamera;
 	checkCudaErrors(cudaMalloc((void**)&_dCamera, sizeof(Camera*)));
+	stop = clock();
+	std::cout << "\t\t" << (stop - start) << "ms\n";
+
 	createWorld<<<1,1>>>(_dList, _dWorld, _dCamera);
 	checkCudaErrors(cudaGetLastError());
 	checkCudaErrors(cudaDeviceSynchronize());
